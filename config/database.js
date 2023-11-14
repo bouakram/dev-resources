bbf3db134228455541d6b3715f85346a5fadf2eb
Akrem Boughazi <kikoua203@gmail.com>
2023-11-14 03:14:02 +0100

    changing db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'sqlite3');
 
   const connections = {
     mysql: {
